55848fcb2281f30027a861c2defecbe95a14fd4e
InvalidSian <103258288+invalidsian@users.noreply.github.com>
2024-08-11 01:31:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
-import colorama
 import os
 import asyncio
-import requests
+import string
+
+try:
+    import colorama
+    import requests
+    from pystyle import Center
+    from webscout import tempid
+except ModuleNotFoundError:
+    os.system("pip install colorama")
+    os.system("pip install requests")
+    os.system("pip install webscout")
+
 import json
 import random
 import re
-
 from datetime import datetime
-from webscout import tempid
 
 colorama.init()
 
 BLACK = colorama.Fore.BLACK
 WHITE = colorama.Fore.WHITE
-CYAN = colorama.Fore.LIGHTCYAN_EX
+ORANGE = "\033[38;2;255;165;0m"
+RESET = colorama.Style.RESET_ALL
+
+ASCII = r"""
+    {o}
+    __    __________________________  ___    _   ______  ____ 
+   / /   /  _/ ____/ ____/ ____/ __ \/   |  / | / / __ \/ __ \
+  / /    / // __/ / /_  / __/ / /_/ / /| | /  |/ / / / / / / /
+ / /____/ // /___/ __/ / /___/ _, _/ ___ |/ /|  / /_/ / /_/ / 
+/_____/___/_____/_/   /_____/_/ |_/_/  |_/_/ |_/_____/\____/{r}"""
+
+CREDITS = r"""
+{o}|{r} Made by: {o}invalidsian
+{o}|{r} UI developed by: {o}vjpe.py
+"""
 
 def get_current_time():
     return datetime.now().strftime('%H:%M:%S')
 
 username = os.environ.get('USERNAME')
 
-text = '''
-                ██╗     ██╗███████╗███████╗███████╗██████╗  █████╗ ███╗   ██╗██████╗  ██████╗ 
-                ██║     ██║██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔═══██╗
-                ██║     ██║█████╗  █████╗  █████╗  ██████╔╝███████║██╔██╗ ██║██║  ██║██║   ██║
-                ██║     ██║██╔══╝  ██╔══╝  ██╔══╝  ██╔══██╗██╔══██║██║╚██╗██║██║  ██║██║   ██║
-                ███████╗██║███████╗██║     ███████╗██║  ██║██║  ██║██║ ╚████║██████╔╝╚██████╔╝
-                ╚══════╝╚═╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ 
-                        Account generator for lieferando.de - made by @invalidsian
-'''
+text = Center.XCenter(ASCII.replace('{o}', ORANGE).replace('{r}', RESET))
+cred = Center.XCenter(CREDITS.replace('{o}', ORANGE).replace('{r}', RESET))
 
-print(CYAN + text)
+print(text)
+print(cred)
 
-print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + f"Hello {username}!")
+print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + f"Hello {username}!")
 
 try:
-    num_accounts = int(input(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "How many accounts would you like to generate? "))
+    num_accounts = int(input(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "How many accounts would you like to generate? "))
     if num_accounts <= 0:
-        print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Invalid number entered. Exiting...")
+        print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Invalid number entered. Exiting...")
         exit(1)
 except ValueError:
-    print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Invalid number entered. Exiting...")
+    print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Invalid number entered. Exiting...")
     exit(1)
-
-random_password = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(12))
 
 BASE_URL_SIGN_UP = "https://cw-api.takeaway.com/api/v34/user/sign_up"
 BASE_URL_LOGIN = "https://cw-api.takeaway.com/api/v34/user/login"
@@ -57,33 +71,42 @@ def get_random_user_agent():
     return random.choice(user_agents).strip()
 
 headers["User-Agent"] = get_random_user_agent()
-print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Using User-Agent: " + headers["User-Agent"])
+print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Using User-Agent: " + headers["User-Agent"])
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def get_random_username():
+    with open("data/usernames.txt", "r") as file:
+        usernames = file.readlines()
+    return random.choice(usernames).strip()
 
 async def create_temp_mail_account(client):
     domains = await client.get_domains()
     if not domains:
-        print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "No domains available. Please try again later.")
+        print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "No domains available. Please try again later.")
         return None, None
 
     email = await client.create_email(domain=domains[0].name)
-    print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Tempmail generated: " + email.email)
-    print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Token for accessing the email: " + email.token)
+    print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Tempmail generated: " + email.email)
+    print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Token for accessing the email: " + email.token)
     return email.email, email.token
 
 async def check_email_messages(client, email):
     while True:
         messages = await client.get_messages(email)
         if messages:
-            print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Checking for 2FA code...")
+            print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Checking for 2FA code...")
             return messages
         await asyncio.sleep(5)
 
-def create_lieferando_account(email):
+def create_lieferando_account(email, name, password):
     while True:
         json_data = {
-            'name': 'Alex',
+            'name': name,
             'email': email,
-            'password': random_password,
+            'password': password,
             'newsletter': False,
         }
         response = requests.post(BASE_URL_SIGN_UP, headers=headers, json=json_data)
@@ -116,16 +139,19 @@ def save_account_data(email, password, response_json):
         file.write(f"{email}:{password} | {response_json}\n")
 
 async def generate_account(client, account_number):
-    print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"Generating {num_accounts} accounts...")
+    print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"Generating {num_accounts} accounts...")
 
     email, token = await create_temp_mail_account(client)
     if not email:
         return
 
-    headers["User-Agent"] = get_random_user_agent()
-    print(BLACK + f"[{get_current_time()}] " + CYAN + ">> " + WHITE + "Using User-Agent: " + headers["User-Agent"])
+    username = get_random_username()
+    password = generate_random_password()
 
-    create_lieferando_account(email)
+    headers["User-Agent"] = get_random_user_agent()
+    print(BLACK + f"[{get_current_time()}] " + ORANGE + ">> " + WHITE + "Using User-Agent: " + headers["User-Agent"])
+
+    create_lieferando_account(email, username, password)
 
     messages = await check_email_messages(client, email)
     if messages:
@@ -133,15 +159,15 @@ async def generate_account(client, account_number):
             code = extract_2fa_code(message.body_text)
             if code:
                 print(f"Your 2FA code: {code}")
-                print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"2FA code found: {code}")
+                print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"2FA code found: {code}")
 
-                response_json = login_lieferando_account(email, random_password, code)
+                response_json = login_lieferando_account(email, password, code)
 
-                save_account_data(email, random_password, response_json)
+                save_account_data(email, password, response_json)
                 
-                print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"Account {account_number}/{num_accounts} generated successfully!")
+                print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"Account {account_number}/{num_accounts} generated successfully!")
             else:
-                print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"No 2FA code found in email.")
+                print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"No 2FA code found in email.")
                 continue
 
 async def main():
@@ -152,10 +178,10 @@ async def main():
             await generate_account(client, i)
 
     except Exception as e:
-        print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"An error occurred: {e}")
+        print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"An error occurred: {e}")
 
     finally:
-        print(BLACK + f"[{get_current_time()}] " + CYAN + f">> " + WHITE + f"Join my discord server: https://discord.gg/RjX2CpdPpd")
+        print(BLACK + f"[{get_current_time()}] " + ORANGE + f">> " + WHITE + f"Join my discord server: https://discord.gg/RjX2CpdPpd")
         await client.close()
 
 if __name__ == '__main__':
